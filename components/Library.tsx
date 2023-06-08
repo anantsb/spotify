cf7d35d499bdc72c9351cf68a41ee03a07f06b33
anantsb <anantbhagat44@gmail.com>
2023-06-08 17:05:59 +0530

    songs fetching and list display functionality

--- components/Library.tsx
+++ components/Library.tsx
@@ -2,8 +2,27 @@
 
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
-const Library = () => {
-  const onClick = () => {};
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "../hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
+
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+  const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    return uploadModal.onOpen();
+  };
   return (
     <div className='flex flex-col'>
       <div className='flex items-center justify-between px-5 pt-4'>
@@ -22,7 +41,11 @@ const Library = () => {
           '
         />
       </div>
-      <div className='flex flex-col gap-y-2 mt-4 px-3'>List of Songs</div>
+      <div className='flex flex-col gap-y-2 mt-4 px-3'>
+        {songs.map((item) => (
+          <MediaItem onClick={() => {}} key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   );
 };
